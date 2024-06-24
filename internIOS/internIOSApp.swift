b7f9c6f0b4f6fede906034301a1ff59e41eb3acf
tungday <nguyenduytungptit1604vy@gmail.com>
2024-06-24 13:28:15 +0700

    Initial Commit

---
+++ internIOS/internIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  internIOSApp.swift
+//  internIOS
+//
+//  Created by Tung on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct internIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
